4dd578302c3e8111fd91155804d987a54b2588c3
David Moore <david.moore45@mail.dcu.ie>
2022-04-12 12:54:20 +0100

    cleaned up lambda_code and pushed dockerfile etc.

--- lambda_code/ocr_lambda/ocr_lambda.py
+++ lambda_code/ocr_lambda/ocr_lambda.py
@@ -3,13 +3,18 @@ import boto3
 import base64
 import pytesseract
 import uuid
+import cv2
 
 client = boto3.client('stepfunctions')
 
 def ocr(img):
-  
+
+  # Preprocessing
+  thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+  result = 255 - thresh
+
   # Perform OCR, remove newlines and convert all chars to lowercase
-  ocr_text = pytesseract.image_to_string(img).strip().replace("\n", " ").lower()
+  ocr_text = pytesseract.image_to_string(result).strip().replace("\n", " ").lower()
   return ocr_text
       
 def ocr_handler(event, context):
@@ -17,15 +22,14 @@ def ocr_handler(event, context):
     # Need a unique ID for triggering step function
     user_id = str(uuid.uuid1())
 
-    # Body from API POST
-    # body = json.loads(event['body'])
-    print(event)
+    # Body from API POST, decode from base64 to json
     body = event['body']
     b64_str = body.encode('utf-8')
     b64_bytes = base64.b64decode(b64_str)
-    decode_str = b64_bytes.decode('utf-')
+    decode_str = b64_bytes.decode('utf-8')
     body = json.loads(decode_str)
     print(body)
+
     body_image64 = body['image64']
     allergies = body['user_allergies']
 
@@ -34,7 +38,8 @@ def ocr_handler(event, context):
       f.write(base64.b64decode(body_image64))
     
     # OCR on saved image
-    ocr_text = ocr("/tmp/saved_img.png")
+    img = cv2.imread("/tmp/saved_img.png",0)
+    ocr_text = ocr(img)
     
     # Wrap the result data as dictionary, use as input for step function
     input = {"user_id": user_id, "ocr_text": ocr_text, "allergies": allergies}
--- lambda_code/test-api.py
+++ lambda_code/test-api.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 api_url = "https://bid4tm2l7g.execute-api.eu-west-1.amazonaws.com/default"
-f = open('ocr_lambda\ocr_test0.json')
+f = open('test_jsons\ocr_test2.json')
 todo = json.load(f)
-response = requests.post(api_url, json=todo)
+response= requests.post(api_url, json=todo)
 print(response.json())
