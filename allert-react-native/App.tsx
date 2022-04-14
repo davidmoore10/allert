3ae157f07460d51232c55e68ece498f18b18e29b
Conor Clerkin <38741780+LemonUnderscore@users.noreply.github.com>
2022-04-14 21:24:47 +0100

    initial implementation of firebase authentication

--- allert-react-native/App.tsx
+++ allert-react-native/App.tsx
@@ -1,8 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import * as React from 'react';
+//navigation components
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//native react components
+import { View, Text } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
+//import screens
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -11,9 +19,12 @@ export default function App() {
   if (!isLoadingComplete) {
     return null;
   }
-  else {
-    return (
-      null
-    );
-  }
+  else return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
---
+++ allert-react-native/firebase.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhqO2V3Ey8m78mfY0TpS_BBpUxKNqCjaw",
+  authDomain: "allert-46d4e.firebaseapp.com",
+  projectId: "allert-46d4e",
+  storageBucket: "allert-46d4e.appspot.com",
+  messagingSenderId: "1044235995442",
+  appId: "1:1044235995442:web:bf54910cd7578d2f602f2b"
+};
+
+// Initialize firebase
+const app = initializeApp(firebaseConfig);
+// initialize auth
+const auth = getAuth();
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged };
