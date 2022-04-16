2d97a5d0f8d76464b688b4e5978501f9ed360a50
Conor Clerkin <38741780+LemonUnderscore@users.noreply.github.com>
2022-04-16 17:06:01 +0100

    replace expo camera with native camera

--- allert-react-native/screens/CameraScreen.tsx
+++ allert-react-native/screens/CameraScreen.tsx
@@ -11,13 +11,6 @@ const CameraScreen = () => {
 	const [hasPermission, setHasPermission] = useState<boolean | null>(null);
 	const [cameraRef, setCameraRef] = useState<Camera | null>(null)
 	const [type, setType] = useState(Camera.Constants.Type.back);
-	
-	//aspect ratio calculations
-	const [imagePadding, setImagePadding] = useState(0);
-	const [ratio, setRatio] = useState('4:3');  // default is 4:3
-	const { height, width } = Dimensions.get('window');
-	const screenRatio = height / width;
-	const [isRatioSet, setIsRatioSet] =  useState(false);
   
 	//on load, check camera permissions
 	useEffect(() => {
@@ -27,66 +20,9 @@ const CameraScreen = () => {
 		})();
 	}, []);
 
-	const setCameraReady = async() => {
-		if (!isRatioSet) {
-		  await prepareRatio();
-		}
-	};
-
-	// set the camera ratio and padding.
-  	// this code assumes a portrait mode screen
-  	const prepareRatio = async () => {
-		let desiredRatio = '4:3';  // Start with the system default
-		// This issue only affects Android
-		if (Platform.OS === 'android') {
-			if (cameraRef) {
-
-				const ratios = await cameraRef.getSupportedRatiosAsync();
-
-				// Calculate the width/height of each of the supported camera ratios
-				// These width/height are measured in landscape mode
-				// find the ratio that is closest to the screen ratio without going over
-				let distances = {};
-				let realRatios = {};
-				let minDistance = null;
-
-				for (const ratio of ratios) {
-
-					const parts = ratio.split(':');
-					const realRatio: number = parseInt(parts[0]) / parseInt(parts[1]);
-					realRatios[ratio] = realRatio;
-
-					// ratio can't be taller than screen, so we don't want an abs()
-					const distance = screenRatio - realRatio; 
-					distances[ratio] = realRatio;
-					
-					if (minDistance == null) {
-						minDistance = ratio;
-					} else {
-						if (distance >= 0 && distance < distances[minDistance]) {
-						minDistance = ratio;
-						}
-					}
-				}
-				// set the best match
-				desiredRatio = minDistance;
-				//  calculate the difference between the camera width and the screen height
-				const remainder = Math.floor(
-				(height - realRatios[desiredRatio] * width) / 2
-				);
-				// set the preview padding and preview ratio
-				setImagePadding(remainder);
-				setRatio(desiredRatio);
-				// Set a flag so we don't do this 
-				// calculation each time the screen refreshes
-				setIsRatioSet(true);
-			}
-		}
-  	};
-
 	const pickImage = async () => {
 		// No permissions request is necessary for launching the image library
-		let result:any = await ImagePicker.launchImageLibraryAsync({
+		let result:any = await ImagePicker.launchCameraAsync({
 		  mediaTypes: ImagePicker.MediaTypeOptions.All,
 		  allowsEditing: true,
 		  aspect: [4, 3],
@@ -100,60 +36,64 @@ const CameraScreen = () => {
 		}
 	  };
 
-	if (hasPermission === null) {
-		return <View />;
-  	}
-  	if (hasPermission === false) {
-		return <Text>No access to camera</Text>;
-  	}
+	const captureImage = async () => {
+	// No permissions request is necessary for launching the image library
+	let result:any = await ImagePicker.launchCameraAsync({
+		mediaTypes: ImagePicker.MediaTypeOptions.All,
+		allowsEditing: true,
+		aspect: [4, 3],
+		quality: 1,
+	});
+
+	console.log(result);
+
+	if (!result.cancelled) {
+		setImage(result.uri);
+	}
+	};
 
 	return (
 		<View style={styles.container}>
-			<Camera
-			style={styles.camera}
-			ratio={ratio}
-			type={type}
-			onCameraReady={setCameraReady}
-			ref={ref => {
-				setCameraRef(ref)
-			}}>
-				<View style={styles.buttonContainer}>
 
+			<View style={styles.buttonContainer}>
+
+				{image ? (
 					<TouchableOpacity
-					style={styles.button}
-					onPress={() => {
-						setType(
-						type === Camera.Constants.Type.back
-							? Camera.Constants.Type.front
-							: Camera.Constants.Type.back
-						);
-					}}>
-						<Text style={styles.text}> Flip </Text>
-					</TouchableOpacity>
-
-					<TouchableOpacity style={{alignSelf: 'flex-end', backgroundColor: "red"}} onPress={async() => {
-						if(cameraRef) {
-							let photo = await cameraRef.takePictureAsync();
-							console.log('photo', photo);
-						}
-					}}>
-						<View style={styles.cameraButton}>
-							<View style={styles.cameraButtonOutline}>
-							</View>
-						</View>
-					</TouchableOpacity>
-
-					<TouchableOpacity
-					onPress={pickImage}
+					onPress={ () => {} }
+					style={[styles.button, styles.buttonOutline]}
 					>
-						<Text>
-						Pick an image from camera roll
-						</Text>
-      					{image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }}/>}
+						<Image source={{ uri: image }} style={{ width: 200, height: 200 }}/>
+						<Text style={styles.buttonOutlineText}>Confirm Image</Text>
 					</TouchableOpacity>
+				) : (
+					<TouchableOpacity
+					onPress={ () => {} }
+					style={[styles.button, styles.buttonOutline]}
+					>
+						<Text style={styles.buttonText}>No image</Text>
+					</TouchableOpacity>
+				)
+				}
 
-				</View>
-			</Camera>
+				<TouchableOpacity
+				onPress={ pickImage }
+				style={styles.button}
+				>
+					<Text style={styles.buttonText}>Capture Image</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+				onPress={ captureImage }
+				style={[styles.button, styles.buttonOutline]}
+				>
+					<Text style={styles.buttonOutlineText}>Choose from Gallery</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+				onPress={ () => {} }
+				style={[styles.button, styles.buttonOutline]}
+				>
+					<Text style={styles.buttonOutlineText}>Help</Text>
+				</TouchableOpacity>
+        	</View>
 		</View>
 	);
 }
@@ -161,45 +101,48 @@ const CameraScreen = () => {
 export default CameraScreen
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 10,
-	justifyContent: "space-between",
-	alignItems: "flex-end",
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  },
-  cameraButton: {
-	borderWidth: 2,
-	borderRadius: 50,
-	borderColor: 'white',
-	height: 50,
-	width:50,
-	display: 'flex',
-	justifyContent: 'center',
-	alignItems: 'center',
-  },
-  cameraButtonOutline: {
-	borderWidth: 2,
-	borderRadius: 50,
-	borderColor: 'white',
-	height: 40,
-	width:40,
-	backgroundColor: 'white',
-  }
+	container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    inputContainer: {
+        width: "80%"
+    },
+    input: {
+        backgroundColor: "white",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: "60%",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 40,
+    },
+    button: {
+        backgroundColor: "#0782f9",
+        width: "100%",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+    },
+    buttonOutline: {
+        backgroundColor: "white",
+        marginTop: 5,
+        borderColor: "#0782f9",
+        borderWidth: 2,
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "700",
+        fontSize: 16,
+    },
+    buttonOutlineText: {
+        color: "#0782f9",
+        fontWeight: "700",
+        fontSize: 16,
+    },
 })
