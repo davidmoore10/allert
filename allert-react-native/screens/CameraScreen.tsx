81fa178b8717a005d234852b774fbf21b8e42a71
Conor Clerkin <38741780+LemonUnderscore@users.noreply.github.com>
2022-04-16 01:19:35 +0100

    added basic expo camera functionality

--- allert-react-native/screens/CameraScreen.tsx
+++ allert-react-native/screens/CameraScreen.tsx
@@ -1,14 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 const CameraScreen = () => {
-  return (
-    <View>
-      <Text>Camera Screen</Text>
-    </View>
-  )
-}
+	const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+	const [type, setType] = useState(Camera.Constants.Type.back);
+  
+	useEffect(() => {
+	  (async () => {
+		const { status } = await Camera.requestCameraPermissionsAsync();
+		setHasPermission(status === 'granted');
+	  })();
+	}, []);
+  
+	if (hasPermission === null) {
+	  return <View />;
+	}
+	if (hasPermission === false) {
+	  return <Text>No access to camera</Text>;
+	}
+	return (
+	  <View style={styles.container}>
+		<Camera style={styles.camera} type={type}>
+		  <View style={styles.buttonContainer}>
+			<TouchableOpacity
+			  style={styles.button}
+			  onPress={() => {
+				setType(
+				  type === Camera.Constants.Type.back
+					? Camera.Constants.Type.front
+					: Camera.Constants.Type.back
+				);
+			  }}>
+			  <Text style={styles.text}> Flip </Text>
+			</TouchableOpacity>
+		  </View>
+		</Camera>
+	  </View>
+	);
+  }
 
 export default CameraScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+})
