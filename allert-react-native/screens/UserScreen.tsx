fb2f33aea7a6b429739f441fb5b66ada5360b677
Conor Clerkin <38741780+LemonUnderscore@users.noreply.github.com>
2022-04-17 22:43:25 +0100

    set up checkbox functionality for user page

--- allert-react-native/screens/UserScreen.tsx
+++ allert-react-native/screens/UserScreen.tsx
@@ -1,10 +1,65 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { auth, signOut } from '../firebase';
-import React from 'react';
-import BouncyCheckbox from "react-native-bouncy-checkbox";
+import React, { useState, useEffect }from 'react';
 import { useNavigation } from '@react-navigation/native';
 
+import Checkbox from 'expo-checkbox';
+
 const UserScreen = () => {
+    const [checkListState, setCheckListState] = useState([
+            {
+                "name": "celery",
+                "enabled": false
+            },
+            {
+                "name": "crustaceans",
+                "enabled": false
+            },
+            {
+                "name": "eggs",
+                "enabled": false
+            },
+            {
+                "name": "fish",
+                "enabled": false
+            },
+            {
+                "name": "gluten",
+                "enabled": false
+            },
+            {
+                "name": "lupin",
+                "enabled": false
+            },
+            {
+                "name": "milk",
+                "enabled": false
+            },
+            {
+                "name": "molluscs",
+                "enabled": false
+            },
+            {
+                "name": "mustard",
+                "enabled": false
+            },
+            {
+                "name": "nuts",
+                "enabled": false
+            },
+            {
+                "name": "sesame seeds",
+                "enabled": false
+            },
+            {
+                "name": "soybeans",
+                "enabled": false
+            },
+            {
+                "name": "sulphites",
+                "enabled": false
+            },
+        ])
 
     const handleLogout = () => {
         signOut(auth).then(
@@ -16,6 +71,26 @@ const UserScreen = () => {
 
     return (
         <View style={styles.container}>
+
+            <View style={styles.buttonContainer}>
+            {checkListState.map((item, index) => (
+                <TouchableOpacity
+                    key={index}
+                    style={[styles.checkbox, item.enabled ? {backgroundColor: "green"} : {backgroundColor:"red"}]}
+                    onPress={ () => {setCheckListState(
+                        checkListState.map((obj, i) => 
+                            i === index ? 
+                            {...obj, "enabled" : !item.enabled} 
+                            : obj 
+                    ))
+                    }
+                }
+                >
+                <Text>{item.name}</Text>
+                </TouchableOpacity>
+      ))}
+                
+            </View>
 
             <View style={styles.buttonContainer}>
                 <TouchableOpacity
@@ -68,4 +143,11 @@ const styles = StyleSheet.create({
         fontWeight: "700",
         fontSize: 16,
     },
+    checkbox: {
+        margin: 8,
+        width: "100%",
+        padding: 5,
+        borderRadius: 10,
+        alignItems: "center",
+      },
 })
